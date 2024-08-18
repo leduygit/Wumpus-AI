634a53b95d1bba971a496f08e751360a16e6220e
leduygit <leduyanh04@gmail.com>
2024-08-18 12:09:24 +0700

    feat: add welcome

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from gui import visualizer
 from logic.simulation import simulate
 
 if __name__ == "__main__":
-    input = input("Do you want to run the simulation? \nThe simulation will take a while to run (y/n): ")
+    print("-" * 50)
+    print("Welcome to the Wumpus World Simulator!")
+    input = input("Do you want to run the simulation? it will take a while to run (y/n): ")
     # lower case the input
     input = input.lower()
     if input == "y":
